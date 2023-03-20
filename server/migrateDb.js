5e7429641689761412bf3f6f7ef674582eaaca27
Theresa Grimus <T.Grimus@gmx.net>
2023-03-20 21:56:17 +0100

    .

--- server/migrateDb.js
+++ server/migrateDb.js
@@ -13,7 +13,6 @@ async function migrateMovies() {
 
   const databaseOfSourceMovies = client.db("sample_mflix");
   const collectionOfSourceMovies = databaseOfSourceMovies.collection("movies");
-  console.log(collectionOfSourceMovies);
   let movies = await collectionOfSourceMovies
     .find({
       poster: { $exists: true },
@@ -54,5 +53,58 @@ async function migrateMovies() {
   }
 }
 
+async function migrateComments() {
+  let skip = 0;
+  const database = client.db("sample_mflix");
+  const collectionOfComments = database.collection("comments");
+
+  let comments = await collectionOfComments
+    .find({})
+    .skip(skip)
+    .limit(10)
+    .toArray();
+  const databaseOfMovies = client.db("cinema");
+  const collectionOfMovies = databaseOfMovies.collection("films");
+  let movies = await collectionOfMovies.find({}).toArray();
+
+  const newComments = databaseOfMovies.collection("comments");
+
+  while (comments.length > 0) {
+    for (const comment of comments) {
+      for (const movie of movies) {
+        if (comment.movie_id.equals(movie._id)) {
+          const newComment = {
+            _id: comment._id,
+            name: comment.name,
+            email: comment.email,
+            movie_id: comment.movie_id,
+            text: comment.text,
+            date: comment.date,
+          };
+          await newComments.insertOne(newComment);
+        }
+      }
+    }
+    skip += 10;
+    comments = await collectionOfComments
+      .find({})
+      .skip(skip)
+      .limit(10)
+      .toArray();
+  }
+}
+
 /* migrateMovies();
  */
+
+/*  migrateComments();
+ */ 
+
+/* async function deleteComments() {
+  const databaseOfMovies = client.db("cinema");
+  const newComments = databaseOfMovies.collection("comments");
+  await newComments.deleteMany();
+
+}
+
+ deleteComments();  */
